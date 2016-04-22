085808012dcae8d9aa4fff26e92a18d111ca139f
Ahammad Reyad <ahammad.reyad@gmail.com>
2016-04-22 17:43:02 +0600

    60830_edit_user_profile

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,5 +1,7 @@
 class Users::RegistrationsController < Devise::RegistrationsController
+  skip_before_filter :require_no_authentication
   before_action :setup_sign_up_params, only: :create
+  before_action :setup_account_update_params, only: :update
 
   def new
     super
@@ -9,8 +11,27 @@ class Users::RegistrationsController < Devise::RegistrationsController
     super
   end
 
+  def edit
+    super
+  end
+
+  def update
+    super
+  end
+
   private
   def setup_sign_up_params
     devise_parameter_sanitizer.for(:sign_up) << :name
+  end
+
+  def setup_account_update_params
+    devise_parameter_sanitizer.for(:account_update) << :name
+  end
+
+  def update_resource resource, params
+    resource.update_without_password(params)
+  end
+
+  def sign_up resource_name, resource
   end
 end
